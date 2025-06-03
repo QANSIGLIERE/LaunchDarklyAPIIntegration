bd1f03b847267e5df0e1fd40dd83b1acae65f388
QANSIGLIERE <qansigliere@gmail.com>
2025-06-03 14:20:34 +0300

    dts files

---
+++ dist/lib/apiIntegration.d.ts
@@ -0,0 +1,21 @@
+export class LaunchDarkly_API {
+    constructor(apiKey?: string);
+    url: string;
+    headers: {
+        'User-Agent': string;
+        'Content-Type': string;
+        Authorization: any;
+    };
+    headersPatch: {
+        'User-Agent': string;
+        'Content-Type': string;
+        Authorization: any;
+    };
+    getContextAttributeNames(projectKey: any, environmentKey: any, additionalFilters?: string): Promise<any>;
+    getListEnvironments(projectKey: any, additionalFilters?: string): Promise<any>;
+    getListFeatureFlags(projectKey: any, additionalFilters?: string): Promise<any>;
+    getContexts(projectKey: any, environmentKey: any, kind: any, key: any, additionalFilters?: string): Promise<any>;
+    postSearchForContexts(projectKey: any, environmentKey: any, jsonBody: any, additionalFilters?: string): Promise<any>;
+    patchUpdateFeatureFlag(projectKey: any, featureFlagKey: any, jsonBody: any, additionalFilters?: string): Promise<any>;
+}
+//# sourceMappingURL=apiIntegration.d.ts.map
---
+++ index.d.ts
@@ -0,0 +1,3 @@
+export { LaunchDarkly_API };
+import { LaunchDarkly_API } from "./dist/lib/apiIntegration";
+//# sourceMappingURL=index.d.ts.map
